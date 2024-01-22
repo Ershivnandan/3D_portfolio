27afdc881700dc856154c277d9412bed844bfab8
ErShivnandan <ec1912@global.org.in>
2024-01-22 21:52:38 +0530

    Website added

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { SpeedInsights } from '@vercel/speed-insights/next';
 
 import App from "./App";
 import "./index.css";
@@ -8,6 +7,5 @@ import "./index.css";
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
-    <SpeedInsights />
   </React.StrictMode>
 );
